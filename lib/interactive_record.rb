237b5ef7e2953244f513073aa0239ea3a9c726b1
Samuel Cornet <samuel@noukod.com>
2018-11-27 18:34:19 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -54,15 +54,6 @@ class InteractiveRecord
 
   def self.find_by **column_names
     sql = "SELECT * FROM #{table_name} WHERE #{column_names.keys[0].to_s} = ? LIMIT 1"
-
     DB[:conn].execute(sql,column_names.values[0].to_s)
   end
-
-# or
-  # def self.find_by **attr
-  #     sql = <<-SQL
-  #     SELECT * FROM #{table_name} WHERE #{attr.keys[0].to_s} = ? LIMIT 1
-  #     SQL
-  #     DB[:conn].execute(sql,attr.values[0].to_s)
-  # end
 end
